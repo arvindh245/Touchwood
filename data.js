e2dc37f0a3e716f8719b84753a65a1037906bcee
arvindh245 <arvindh8055@gmail.com>
2025-05-17 10:49:46 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,185 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dining",
+      "name": "dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5472847738787152,
+        "pitch": 0.2813259809024302,
+        "fov": 1.2996675116209502
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9291856212927456,
+          "pitch": 0.11342181108300053,
+          "rotation": 11.780972450961727,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": 0.8738064194143647,
+          "pitch": 0.1612123149306015,
+          "rotation": 5.497787143782138,
+          "target": "3-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dining1",
+      "name": "dining1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.977710586996386,
+          "pitch": 0.24088925396417693,
+          "rotation": 0.7853981633974483,
+          "target": "3-living"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.8342998724550004,
+        "pitch": 0.3379374364439016,
+        "fov": 1.2996675116209502
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7362443520495958,
+          "pitch": 0.22649734331042382,
+          "rotation": 0.7853981633974483,
+          "target": "1-dining1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-living",
+      "name": "living",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.4846683451400224,
+        "pitch": 0.02606518102002653,
+        "fov": 1.2996675116209502
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5490226990390425,
+          "pitch": 0.07248907704586927,
+          "rotation": 0,
+          "target": "0-dining"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "A7-Eeshanya",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
